fcc5714626c38a1cf9993c0481dfc9ce9a6aa241
Sanan Ibrahimoff <36265361+senanito85@users.noreply.github.com>
2024-11-18 11:55:20 +1100

    Update main.go
    remove comments

--- main.go
+++ main.go
@@ -11,22 +11,22 @@ func main() {
 
 	// Redirect the root path ("/") to the "/home" path
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/home", http.StatusFound) // StatusFound = 302
+		http.Redirect(w, r, "/home", http.StatusFound) 
 	})
 
 	// Serve the "home.html" file at the "/home" path
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./home.html") // Ensure the file path is correct
+		http.ServeFile(w, r, "./home.html") 
 	})
 
 	// Serve the "info.html" file at the "/info" path
 	http.HandleFunc("/info", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./info.html") // Ensure the file path is correct
+		http.ServeFile(w, r, "./info.html") 
 	})
 
 	// Serve the "contacts.html" file at the "/contacts" path
 	http.HandleFunc("/contacts", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./contacts.html") // Ensure the file path is correct
+		http.ServeFile(w, r, "./contacts.html") 
 	})
 
 	// Start the server
